045e2ea4efa3a11ee85b15835c89e05ab0c2fcd1
ipopadic-ip <popadic2003@gmail.com>
2025-09-16 15:03:45 +0200

    Initial Commit

---
+++ 1. Uvod u mašinsko ucenje i kompjutersku viziju/zadatak/README.md
@@ -0,0 +1,11 @@
+Na raspolaganju imate mikroskopske slike uzorka krvi. 
+Promene u broju ili odnosu ovih ćelija mogu biti pokazatelj različitih stanja organizma.
+
+Na primer:
+- smanjen broj eritrocita može ukazivati na anemiju,
+- povećan broj leukocita može biti znak infekcije, zapaljenja ili hematoloških bolesti,
+- drastično promenjen odnos belih i crvenih krvnih zrnaca može biti jedan od pokazatelja leukemije.
+
+
+Vaš zadatak je da, koristeći metode računarske vizije, automatski prebrojite crvena krvna zrnca (eritrocite) na svakoj slici.
+Rezultat vaše funkcije treba da bude ukupan broj detektovanih eritrocita za datu sliku.
---
+++ 1. Uvod u mašinsko ucenje i kompjutersku viziju/zadatak/main.py
@@ -0,0 +1,23 @@
+from process import count_blood_cells
+import glob
+import sys
+import os
+
+DATASET_PATH = '.'+os.path.sep+'dataset'+os.path.sep
+
+processed_image_names = []
+red_blood_cell_counts = []
+
+for image_path in glob.glob(DATASET_PATH + "*.bmp"):
+    image_directory, image_name = os.path.split(image_path)
+    processed_image_names.append(image_name)
+    red_blood_cell_counts.append(count_blood_cells(image_path))
+
+
+result_file_contents = ""
+for image_index, image_name in enumerate(processed_image_names):
+    result_file_contents += "%s,%s\n" % (image_name, red_blood_cell_counts[image_index])
+
+# čuvanje rezultata u csv fajl
+with open('result.csv', 'w') as output_file:
+    output_file.write(result_file_contents)
