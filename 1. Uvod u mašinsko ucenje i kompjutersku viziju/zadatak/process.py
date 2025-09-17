dbab08f7fe0e0f198b4ae1bc32edaa686384198b
ipopadic-ip <popadic2003@gmail.com>
2025-09-17 15:13:31 +0200

    2nd Day

--- 1. Uvod u mašinsko ucenje i kompjutersku viziju/zadatak/process.py
+++ 1. Uvod u mašinsko ucenje i kompjutersku viziju/zadatak/process.py
@@ -4,7 +4,7 @@ import numpy as np
 def count_blood_cells(image_path):
     """
     Procedura prima putanju do fotografije i vraca broj crvenih krvnih zrnaca.
-    Poboljšana verzija sa adaptivnim pragovima i boljom detekcijom boja.
+    Prilagodjena za svetlo i tamno roze/crevene eritrocite.
     """
 
     # Učitavanje slike
@@ -15,56 +15,46 @@ def count_blood_cells(image_path):
     # Kopija originalne slike za prikaz
     result_img = img.copy()
     
-    # 1. PREDOBRADA SLIKE
-    # Smanjenje šuma
+    # 1. PREDOBRADA SLIKE - smanjenje šuma
     blurred = cv2.GaussianBlur(img, (5, 5), 0)
     
-    # 2. KONVERZIJA U RAZLIČITE PROSTORE BOJA
-    rgb = cv2.cvtColor(blurred, cv2.COLOR_BGR2RGB)
+    # 2. KONVERZIJA U HSV (bolja za detekciju boja)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
     
-    # 3. DETEKCIJA ERITROCITA KROZ VIŠE PRISTUPA
-    # Prvi pristup: RGB - crveni kanal
-    r_channel = rgb[:,:,0]
-    _, mask_r = cv2.threshold(r_channel, 100, 255, cv2.THRESH_BINARY)
+    # 3. DEFINISANJE OPSEGA BOJA ZA ERITROCITE
+    # Svetlo roze (manje zasicenje)
+    lower_pink = np.array([160, 30, 150])
+    upper_pink = np.array([179, 100, 255])
     
-    # Drugi pristup: HSV - crvena boja (oba opsega)
-    lower_red1 = np.array([0, 50, 50])
-    upper_red1 = np.array([10, 255, 255])
-    lower_red2 = np.array([160, 50, 50])
-    upper_red2 = np.array([180, 255, 255])
+    # Tamnije roze/crvena (visa saturacija)
+    lower_red = np.array([0, 50, 150])
+    upper_red = np.array([15, 255, 255])
     
-    mask_hsv1 = cv2.inRange(hsv, lower_red1, upper_red1)
-    mask_hsv2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask_hsv = cv2.bitwise_or(mask_hsv1, mask_hsv2)
+    # 4. KREIRANJE MASKİ
+    mask_pink = cv2.inRange(hsv, lower_pink, upper_pink)
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
     
-    # Treći pristup: LAB - a kanal (crveno-zelena osa)
-    a_channel = lab[:,:,1]
-    _, mask_a = cv2.threshold(a_channel, 130, 255, cv2.THRESH_BINARY)
-    
-    # 4. KOMBINOVANJE MASKİ
-    combined_mask = cv2.bitwise_and(mask_r, mask_hsv)
-    combined_mask = cv2.bitwise_and(combined_mask, mask_a)
+    # Kombinovanje maski
+    combined_mask = cv2.bitwise_or(mask_pink, mask_red)
     
     # 5. ZELENA POZADINA za vizuelizaciju
     green_bg = img.copy()
     green_bg[combined_mask == 0] = (0, 255, 0)
     
-    # 6. MORFOLOŠKE OPERACIJE za čišćenje
+    # 6. MORFOLOŠKE OPERACIJE za čišćenje maske
     kernel = np.ones((3, 3), np.uint8)
     
     # Otvaranje za uklanjanje šuma
     cleaned_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel, iterations=2)
     
-    # Zatvaranje za popunjavanje rupa
+    # Zatvaranje za popunjavanje rupa u eritrocitima
     cleaned_mask = cv2.morphologyEx(cleaned_mask, cv2.MORPH_CLOSE, kernel, iterations=3)
     
     # 7. PRONALAŽENJE KONTURA
     contours, _ = cv2.findContours(cleaned_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     blood_cell_count = 0
-    min_area = 30
+    min_area = 50
     max_area = 1500
     
     for i, cnt in enumerate(contours):
@@ -76,7 +66,7 @@ def count_blood_cells(image_path):
                 circularity = 4 * np.pi * area / (perimeter * perimeter)
                 
                 # Eritrociti su približno kružni
-                if 0.4 < circularity < 1.6:
+                if 0.5 < circularity < 1.5:
                     blood_cell_count += 1
                     
                     # Oznaka detektovanih eritrocita
@@ -97,9 +87,8 @@ def count_blood_cells(image_path):
     # Prikaz svih koraka za debagovanje
     cv2.imshow("Originalna slika", img)
     cv2.imshow("Zelena pozadina", green_bg)
-    cv2.imshow("RGB Mask", mask_r)
-    cv2.imshow("HSV Mask", mask_hsv)
-    cv2.imshow("LAB Mask", mask_a)
+    cv2.imshow("Pink Mask", mask_pink)
+    cv2.imshow("Red Mask", mask_red)
     cv2.imshow("Kombinovana maska", combined_mask)
     cv2.imshow("Ociscena maska", cleaned_mask)
     cv2.imshow("Rezultat detekcije", result_img)
